b523912bce4cbcccd3983a3576a5506a029afa9c
sunasdchang <1729619432@qq.com>
2018-12-29 13:11:19 +0800

    首页功能

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-##很好的代码
+##代码提示
+#.制作首页app组件
+1.完成header区域，使用的是Mint-UI中的header组件
+2.制作底部tabbar区域，使用的是MUI的tabbar在制作购物车小图标的时候，作会多些拷入css样式，拷入ttf文件，到项目中购物车小图标添加样式
+3.要在中间放置一个router-view来展示路由匹配到的组件
+
+#改造tabbar为router-link
+
+#设置路由高亮
+linkActiveClass: 'mui-active'
+
+#点击tabbar中的路由链接，展示对应的路由组件
+
+#制作首页轮播图布局
+1.获取数据，如何获取,使用vue-resource装cnpm i vue-resource -S
+2.s使用vue-resource 的this.$http.get获取数据
+3.获取到的数据，要保存到data身上
+4.使用v-for循环渲染每个item项
--- src/main.js
+++ src/main.js
@@ -1,15 +1,34 @@
 // 入口文件
 import Vue from 'vue'
+//导入路由的包
+import VueRouter from 'vue-router'
+//安装路由
+Vue.use(VueRouter)
+
+//导入vue-resource
+import VueResource from 'vue-resource'
+// 安装vue-resource
+Vue.use(VueResource)
+
 //导入MUI的样式
 import './lib/mui/css/mui.min.css'
-//按需导入组件
-import { Header } from 'mint-ui'
-Vue.component(Header.name, Header)
+import './lib/mui/css/icons-extra.css'
+
+//按需导入Mint-UI组件
+import { Header, Swipe, SwipeItem } from 'mint-ui'
+Vue.component(Header.name, Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+
+//导入自己的router.js路由模块
+import router from './router.js'
 
 //导入app根组件
 import app from './App.vue'
 
 var vm = new Vue({
     el: '#app',
-    render: c => c(app)
+    render: c => c(app),
+    router//挂载路由对象到vm实例上
 })
